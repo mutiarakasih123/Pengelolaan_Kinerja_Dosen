705d2eb00744a1e8018cae0747ccccc85aeee773
alan365 <acepalan3@gmail.com>
2020-03-22 18:49:30 +0700

    add template, dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('template')
+
+@section('konten')
+
+<h1 class="cover-heading text-center">Selamat Datang</h1>
+<h3 class="cover-heading text-center">Di Aplikasi Pengelolaan Kinerja Dosen</h3>
+<p class="lead p-4 text-center">
+    Aplikasi ini untuk............................isi deskripsi aplikasi disini tiara
+</p>
+    
+@endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,3 +1,5 @@
+<?php
+    if (session('userId') !== null) { ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,12 +9,16 @@
     <title>PKD</title>
     <link href="/assets/css/bootstrap4.4.min.css" rel="stylesheet"/>
     <link href="/assets/css/dashboard.css" rel="stylesheet"/>
+    <link href="/assets/fontawesome-free/css/all.css" rel="stylesheet"/>
+    <link href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
     
 </head>
 <body>
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
         <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#">Pengelolaan Kinerja Dosen</a>
             {{-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> --}}
+        <h5 style="color: #f8f9fa;">Welcome Back <?=session('email')?> </h5>
         <ul class="navbar-nav px-3">
             <li class="nav-item text-nowrap">
                 <button type="button" class="btn btn-outline-danger">Sign out</button>
@@ -27,72 +33,33 @@
                 <div class="sidebar-sticky">
                     <ul class="nav flex-column">
                         <li class="nav-item">
-                            <a class="nav-link active" href="home">
+                            <a class="nav-link active" href="/home">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-                            Dashboard <span class="sr-only">(current)</span>
+                            Dashboard
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
-                            Orders
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
-                            Products
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-                            Customers
+                            <a class="nav-link" href="/users">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+                            Users
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bar-chart-2"><line x1="18" y1="20" x2="18" y2="10"></line><line x1="12" y1="20" x2="12" y2="4"></line><line x1="6" y1="20" x2="6" y2="14"></line></svg>
-                            Reports
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
-                            Integrations
-                            </a>
-                        </li>
-                    </ul>
-
-                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                        <span>Saved reports</span>
-                        <a class="d-flex align-items-center text-muted" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
-                        </a>
-                    </h6>
-                    <ul class="nav flex-column mb-2">
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
-                            Current month
+                            Kegiatan Dosen
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
-                            Last quarter
+                            Pelaksanaan Pendidikan
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
-                            Social engagement
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://getbootstrap.com/docs/4.0/examples/dashboard/#">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
-                            Year-end sale
+                            Membina Kegiatan
                             </a>
                         </li>
                     </ul>
@@ -229,11 +196,7 @@
                     </tbody>
                     </table>
                 </div> --}}
-                <h1 class="cover-heading text-center">Selamat Datang</h1>
-                <h3 class="cover-heading text-center">Di Aplikasi Pengelolaan Kinerja Dosen</h3>
-                <p class="lead p-4 text-center">
-                    Aplikasi ini untuk............................isi deskripsi aplikasi disini tiara
-                </p>
+                @yield('konten')
             </main>
         </div>
     </div>
@@ -241,7 +204,15 @@
     <script src="/assets/js/jquery-3.4.1.slim.min.js"></script>
     <script src="/assets/js/popper.min.js"></script>
     <script src="/assets/js/bootstrap.min.js"></script>
-    <script src="/assets/js/leaflet.js"></script>
-    <script src="/assets/js/my-map.js"></script>
+    <script src="/assets/js/custom.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    <script src="/assets/fontawesome-free/js/all.js"></script>
 </body>
 </html>
+
+<?php
+    } else {
+        return redirect()->to('')->send();
+    }
+?>
